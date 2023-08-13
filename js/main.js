fe89413794d3948f5b592057cecb5c9dc1876165
akn774 <katsu4509.ngu@gmail.com>
2023-08-13 15:37:17 +0900

    show json data

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 fetch ('./data/character.json')
   .then (response => response.json ())
-  .then (data => {
-    console.log ('JSONファイルの内容:');
-    console.log (data);
+  .then (result => {
+    // 装備アイテムを表示するための要素を、配列で取得する
+    // equipment_list = document.querySelector ('.equipment').childNodes;
+
+    body = document.querySelector ('body');
+    body.insertAdjacentElement ('afterbegin', result);
+
+    // 各要素に値を突っ込む
+    // element.insertAdjacentElement('afterbegin', image);
   })
   .catch (error => {
     console.error ('エラー:', error);
